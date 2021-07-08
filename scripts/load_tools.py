10a2095d9da718876ea021b2b61291e16803fd42
malodetz <malo.detz@yandex.ru>
2021-07-08 22:54:53 +0300

    Fixing code2seq

--- scripts/load_tools.py
+++ scripts/load_tools.py
@@ -15,13 +15,19 @@ def setup_psiminer() -> None:
         os.chdir("..")
 
 
+def add_path_code2seq() -> None:
+    sys.path.append("./code2seq/code2seq")
+    sys.path.append("./code2seq/code2seq/dataset")
+
+
 def setup_code2seq() -> None:
     """Load code2seq if needed and add it to path"""
 
-    if not os.path.isdir("сode2seq"):
+    if not os.path.isdir("code2seq"):
         link = "https://github.com/JetBrains-Research/code2seq.git"
-        git.Repo.clone_from(link, "./сode2seq", multi_options=["--depth 1 -b test-results-serialization"])
-    sys.path.append("./сode2seq/code2seq")
+        git.Repo.clone_from(link, "./code2seq", multi_options=["--depth 1 -b test-results-serialization"])
+    add_path_code2seq()
+    print(sys.path)
 
 
 if __name__ == "__main__":
