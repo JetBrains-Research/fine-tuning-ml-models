1f304a599f835c2bc6038e53df88b7205b62fd17
malodetz <malo.detz@yandex.ru>
2021-07-21 00:53:52 +0300

    Cross-validation added

--- scripts/fine_tune.py
+++ scripts/fine_tune.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import os
+import tempfile
 from typing import Tuple
 
 import torch
@@ -15,6 +16,8 @@ from code2seq.dataset import PathContextDataModule, TypedPathContextDataModule
 from code2seq.model import Code2Seq, Code2Class, TypedCode2Seq
 from code2seq.utils.callback import UploadCheckpointCallback, PrintEpochResultCallback
 from code2seq.utils.vocabulary import Vocabulary
+
+from .test_single import test_single
 
 
 def load_code2seq(
@@ -44,7 +47,9 @@ def load_typed_code2seq(
 KNOWN_MODELS = {"code2seq": load_code2seq, "code2class": load_code2class, "typed-code2seq": load_typed_code2seq}
 
 
-def fine_tune(dataset_path: str, model_path: str, folds_number: int) -> None:
+def train_and_test(dataset_path: str, model_path: str, fold_idx: int) -> str:
+    """Trains model and return a path to best checkpoint"""
+
     project_name = os.path.basename(os.path.normpath(dataset_path))
 
     checkpoint = torch.load(model_path, map_location=torch.device("cpu"))
@@ -54,13 +59,13 @@ def fine_tune(dataset_path: str, model_path: str, folds_number: int) -> None:
 
     if config.name not in KNOWN_MODELS:
         print(f"Unknown model {config.name}, try one of {' '.join(KNOWN_MODELS.keys())}")
-        return
+        return ""
 
     model, data_module = KNOWN_MODELS[config.name](model_path, config, vocabulary)
 
     # define model checkpoint callback
     checkpoint_callback = ModelCheckpoint(
-        dirpath=os.path.join("models", "fine_tuned", project_name),
+        dirpath=os.path.join("models", "fine_tuned", project_name, str(fold_idx)),
         period=config.save_every_epoch,
         save_top_k=-1,
     )
@@ -93,6 +98,35 @@ def fine_tune(dataset_path: str, model_path: str, folds_number: int) -> None:
     trainer.fit(model=model, datamodule=data_module)
     trainer.test()
 
+    return checkpoint_callback.best_model_path
+
+
+def fine_tune(dataset_path: str, model_path: str, folds_number: int):
+    dataset = open(os.path.join(dataset_path, "java-med-psi-no-types", "java-med-psi-no-types.test.c2s"), "r")
+    samples = dataset.readlines()
+    fold_size = len(samples) // folds_number
+
+    with tempfile.TemporaryDirectory(dir=".") as tmp, open("results.txt", "w") as result_file:
+        for i in range(folds_number - 1):
+            preprocessed_path = os.path.join(tmp, str(i + 1))
+            fold_path = os.path.join(preprocessed_path, "java-med-psi-no-types")
+            os.makedirs(fold_path)
+
+            with open(os.path.join(fold_path, "java-med-psi-no-types.train.c2s"), "w+") as train:
+                train.writelines(samples[: i * fold_size])
+                train.writelines(samples[(i + 2) * fold_size:])
+
+            with open(os.path.join(fold_path, "java-med-psi-no-types.val.c2s"), "w+") as val:
+                val.writelines(samples[(i + 1) * fold_size: (i + 2) * fold_size])
+
+            with open(os.path.join(fold_path, "java-med-psi-no-types.test.c2s"), "w+") as test:
+                test.writelines(samples[i * fold_size: (i + 1) * fold_size])
+
+            print(f"Fold #{i}:", file=result_file)
+            print("Metrics before:", test_single(model_path, preprocessed_path), file=result_file)
+            trained_model_path = train_and_test(preprocessed_path, model_path, i)
+            print("Metrics after:", test_single(trained_model_path, preprocessed_path), file=result_file)
+
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
@@ -101,4 +135,4 @@ if __name__ == "__main__":
     arg_parser.add_argument("folds", type=int, help="Number of folds for k-fold cross-validation")
 
     args = arg_parser.parse_args()
-    print(fine_tune(args.project, args.model, args.folds))
+    fine_tune(args.project, args.model, args.folds)
