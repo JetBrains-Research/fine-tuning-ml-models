053932b5c2e22c0091a1572ec03072d0e6c251dd
malodetz <malo.detz@yandex.ru>
2021-08-27 10:35:32 +0300

    Fixes for textual metrics

--- experiments/fine_tune_and_calc_results.py
+++ experiments/fine_tune_and_calc_results.py
@@ -4,7 +4,7 @@ import os
 from scripts.fine_tune import train_and_test
 from scripts.dump_results import extract
 from scripts.save_metrics import calculate_and_dump_metrics
-from scripts.utils import PREPROCESSED_DATASETS_DIR, RESULTS_DIR, EXPERIMENT_MODEL_DIR
+from scripts.utils import PREPROCESSED_DATASETS_DIR, RESULTS_DIR, EXPERIMENT_MODEL_DIR, CODE2SEQ_VOCABULARY
 
 
 def run_models_and_save_results(project_name: str, model_path: str) -> None:
@@ -28,11 +28,13 @@ def run_models_and_save_results(project_name: str, model_path: str) -> None:
     trained_path, trained_before, trained_after = train_and_test(dataset_path, trained_model_folder, model_path)
     with open(os.path.join(result_folder, "trained_before.jsonl"), "w") as f:
         print(*trained_before, file=f)
-    trained_before_names = extract(model_path, dataset_path, os.path.join(result_folder, "trained_before_names.txt"))
+    trained_before_names = extract(model_path, dataset_path, CODE2SEQ_VOCABULARY,
+                                   os.path.join(result_folder, "trained_before_names.txt"))
     calculate_and_dump_metrics(trained_before_names, os.path.join(result_folder, "trained_before_metrics.csv"))
     with open(os.path.join(result_folder, "trained_after.jsonl"), "w") as f:
         print(*trained_after, file=f)
-    trained_after_names = extract(trained_path, dataset_path, os.path.join(result_folder, "trained_after_names.txt"))
+    trained_after_names = extract(trained_path, dataset_path, CODE2SEQ_VOCABULARY,
+                                  os.path.join(result_folder, "trained_after_names.txt"))
     calculate_and_dump_metrics(trained_after_names, os.path.join(result_folder, "trained_after_metrics.csv"))
 
 
