a9bffad93459b71b6caa0f3eb5f3df7a56a91b25
malodetz <malo.detz@yandex.ru>
2022-04-10 22:01:20 +0300

    Remove threshold

--- scripts/preprocessing/projects_to_c2s.py
+++ scripts/preprocessing/projects_to_c2s.py
@@ -8,14 +8,10 @@ from scripts.utils import PSIMINER_CODE2SEQ_TOPIC_CONFIG
 def process_separate(dataset_path: str) -> None:
     for projects_set in ["training", "validation", "test"]:
         projects_set_folder = os.path.join(dataset_path, projects_set)
-        cnt = 0
         for name in os.listdir(projects_set_folder):
             input_path = os.path.join(projects_set_folder, name)
             output_path = os.path.join("separate_lines", projects_set, name)
             run_psiminer(input_path, output_path, PSIMINER_CODE2SEQ_TOPIC_CONFIG)
-            cnt += 1
-            if cnt == 5:
-                break
 
 
 if __name__ == "__main__":
