d3b2afe96daea44100a98543f7fcaaa051601dc2
malodetz <malo.detz@yandex.ru>
2022-04-10 23:56:35 +0300

    Preliminary concatenation

---
+++ scripts/preprocessing/concat_c2s_to_folds.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+import os
+import json
+from scripts.utils import PREPROCESSED_DATASETS_DIR
+
+
+def unite_by_topics(data_path: str, topics_number: int) -> None:
+    with open(data_path, "r") as f:
+        data = json.load(f)[str(topics_number)]
+    for i in range(topics_number):
+        output_dir = os.path.join(PREPROCESSED_DATASETS_DIR, f"{topics_number}_{i}")
+        os.mkdir(output_dir)
+        for projects_set, short_name in zip(["training", "validation", "test"], ["train", "val", "test"]):
+            projects = [k for k, v in data[projects_set].items() if v == i]
+            with open(os.path.join(output_dir, f"{short_name}.c2s"), "w") as f:
+                for project in projects:
+                    print(project)
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("topics_distribution_path", type=str, help="Path to file with info about project topics")
+    arg_parser.add_argument("number_of_topics", type=int, help="Number of project topics")
+    args = arg_parser.parse_args()
+
+    unite_by_topics(args.topics_distribution_path, args.number_of_topics)
--- scripts/preprocessing/projects_to_c2s.py
+++ scripts/preprocessing/projects_to_c2s.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 import os
 
 from .preprocess import run_psiminer
-from scripts.utils import PSIMINER_CODE2SEQ_TOPIC_CONFIG
+from scripts.utils import PSIMINER_CODE2SEQ_CONFIG
 
 
 def process_separate(dataset_path: str) -> None:
@@ -11,7 +11,7 @@ def process_separate(dataset_path: str) -> None:
         for name in os.listdir(projects_set_folder):
             input_path = os.path.join(projects_set_folder, name)
             output_path = os.path.join("separate_lines", projects_set, name)
-            run_psiminer(input_path, output_path, PSIMINER_CODE2SEQ_TOPIC_CONFIG)
+            run_psiminer(input_path, output_path, PSIMINER_CODE2SEQ_CONFIG)
 
 
 if __name__ == "__main__":
